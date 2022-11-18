c2982c64cbfc6b5d5df8e06f20eea0aa7c052120
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-11-18 20:55:03 +0100

    Can now write numbers into the calculator, use the dot and clear the input

--- script.js
+++ script.js
@@ -9,6 +9,33 @@ const backspaceBtn = document.getElementById('backspace');
 const equalsBtn = document.getElementById('equals');
 const dotBtn = document.getElementById('dot');
 
+const userInput = document.querySelector('.user-input');
+
+numbers.forEach(num =>
+  num.addEventListener('click', () => appendNum(num.textContent))
+)
+
+cancelBtn.addEventListener('click', resetInput);
+dotBtn.addEventListener('click', useDot);
+
+function appendNum(num){
+    userInput.textContent += num;
+}
+
+function resetInput(){
+    userInput.textContent = '';
+}
+
+function useDot(){
+    if (userInput.textContent === ''){
+        userInput.textContent = '0';
+    }
+    if (userInput.textContent.includes('.')){
+        return
+    }  
+    userInput.textContent += '.'
+}
+
 buttons.forEach(btn => btn.addEventListener('click', function(e){
     console.log(e.target);
 }));
