cf5a057990236168dfcb5bda1324ea7e27e1a097
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-11-18 20:34:41 +0100

    Added hover and click effect to buttons and made variables for each button type in script

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const btnContainer = document.querySelector('.btns-container');
 
 
+const buttons = document.querySelectorAll('.btn');
+const numbers = document.querySelectorAll('[data-type="number"]');
+const operators = document.querySelectorAll('[data-type="operator"]');
+const cancelBtn = document.getElementById('ac');
+const backspaceBtn = document.getElementById('backspace');
+const equalsBtn = document.getElementById('equals');
+const dotBtn = document.getElementById('dot');
 
-calcButtons();
+buttons.forEach(btn => btn.addEventListener('click', function(e){
+    console.log(e.target);
+}));
+
+
