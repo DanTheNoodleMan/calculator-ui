a1a3e2f08331974a2ad26ffc8685211057881cc4
Daniel Robertson <danielrobertsonbusiness@gmail.com>
2022-11-20 14:22:27 +0100

    Html: changed screen names to make more readable, finished calculator functionality evaluating a pair of operands at a time and outputing the result onto a different part of the screen

--- script.js
+++ script.js
@@ -1,59 +1,108 @@
 const btnContainer = document.querySelector('.btns-container');
 
 
-const buttons = document.querySelectorAll('.btn');
 const numbers = document.querySelectorAll('[data-type="number"]');
 const operators = document.querySelectorAll('[data-type="operator"]');
-const cancelBtn = document.getElementById('ac');
+const acBtn = document.getElementById('ac');
 const backspaceBtn = document.getElementById('backspace');
 const equalsBtn = document.getElementById('equals');
 const dotBtn = document.getElementById('dot');
 
-const userInput = document.querySelector('.user-input');
+const lastOpScreen = document.querySelector('.last-op');
+const currentOpScreen = document.querySelector('.current-op');
 
-numbers.forEach(num =>
-  num.addEventListener('click', () => appendNum(num.textContent))
-)
 
+let operand1 = '';
+let operand2 = '';
+let operator = null;
+
+let needToReset = false;
 //BUTTON EVENT LISTENERS
-cancelBtn.addEventListener('click', resetInput);
+
+numbers.forEach(num => num.addEventListener('click', () => appendNum(num.textContent)))
+operators.forEach(op => op.addEventListener('click', () => setOperator(op)))
+
+acBtn.addEventListener('click', ac);
 dotBtn.addEventListener('click', useDot);
 backspaceBtn.addEventListener('click', backspace);
+equalsBtn.addEventListener('click', myEval);
 
 
 //BUTTON FUNCTIONS
 function appendNum(num){
-    if(userInput.textContent === '0'){
+    if(currentOpScreen.textContent === '0' || needToReset){
         console.log(true);
         resetInput();
     }
-    userInput.textContent += num;
+    currentOpScreen.textContent += num;
 }
 
 function resetInput(){
-    userInput.textContent = '';
+    currentOpScreen.textContent = '';
+    needToReset = false;
+}
+
+function ac(){
+    operand1='';
+    operand2='';
+    operator = null;
+    lastOpScreen.textContent = '';
+    currentOpScreen.textContent = '';
 }
 
 function useDot(){
-    if (userInput.textContent === ''){
-        userInput.textContent = '0';
+    if (currentOpScreen.textContent === ''){
+        currentOpScreen.textContent = '0';
     }
-    if (userInput.textContent.includes('.')){
+    if (currentOpScreen.textContent.includes('.')){
         return
     }  
-    userInput.textContent += '.'
+    currentOpScreen.textContent += '.'
 }
 
 function backspace() {
-    userInput.textContent = userInput.textContent
+    currentOpScreen.textContent = currentOpScreen.textContent
       .toString()
       .slice(0, -1)
   }
 
+
+function setOperator(op){
+    if(operator !== null) myEval();
+    operand1 = currentOpScreen.textContent;
+    operator = op.classList[0];
+    opSymbol = translateOperator(operator);
+    lastOpScreen.textContent = `${operand1} ${opSymbol}`;
+    needToReset = true;
+}
+
+
+function translateOperator(op){
+    switch(op){
+        case 'plus':
+            return '+';
+        case 'minus':
+            return '-';
+        case 'multiply':
+            return '×';
+        case 'division':
+            return '÷';
+        case 'percentage':
+            return '%';
+        default:
+            return null;
+    }
+}
 //Operation functions
 
 function myEval(){
+    if(operator === null || needToReset) return;
     
+    operand2 = currentOpScreen.textContent;
+    currentOpScreen.textContent = operation(operator, operand1, operand2);
+    lastOpScreen.textContent = `${operand1} ${translateOperator(operator)} ${operand2} =`;
+    operator = null;
+    needToReset =  true;
 }
 
 function sum(a,b){
@@ -81,23 +130,18 @@ function operation(op, a, b){
     a = Number(a);
     b = Number(b);
     switch(op){
-        case '+':
+        case 'plus':
             return sum(a,b);
-        case '-':
+        case 'minus':
             return minus(a,b);
-        case '*':
+        case 'multiply':
             return multiply(a,b);
-        case '/':
+        case 'division':
             return divide(a,b);
-        case '%':
+        case 'percentage':
             return percentage(a,b);
         default:
             return null;
     }
 }
-
-buttons.forEach(btn => btn.addEventListener('click', function(e){
-    console.log(e.target);
-}));
-
 
